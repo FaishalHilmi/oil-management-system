e20bdf9b210f2e4928584a46cb3e8d1db3cad86f
Faisal Hilmi <faisalhilmi082@gmail.com>
2026-06-29 22:43:34 +0700

    feat(layout): create application layout

--- src/routes/AppRouter.tsx
+++ src/routes/AppRouter.tsx
@@ -4,15 +4,18 @@ import DashboardPage from "@/pages/DashboardPage";
 import TransactionPage from "@/pages/TransactionPage";
 import PartnerPage from "@/pages/PartnerPage";
 import ReportPage from "@/pages/ReportPage";
+import AppLayout from "@/layouts/AppLayout";
 
 export default function AppRouter() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<DashboardPage />} />
-        <Route path="/transactions" element={<TransactionPage />} />
-        <Route path="/partners" element={<PartnerPage />} />
-        <Route path="/reports" element={<ReportPage />} />
+        <Route element={<AppLayout />}>
+          <Route path="/" element={<DashboardPage />} />
+          <Route path="/transactions" element={<TransactionPage />} />
+          <Route path="/partners" element={<PartnerPage />} />
+          <Route path="/reports" element={<ReportPage />} />
+        </Route>
       </Routes>
     </BrowserRouter>
   );
