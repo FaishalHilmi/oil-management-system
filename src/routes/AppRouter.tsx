febaaefbedd9d5d9ac884a5bee17f7c2c96748ab
Faisal Hilmi <faisalhilmi082@gmail.com>
2026-06-30 13:53:18 +0700

    refactor(router): restructure page routing

--- src/routes/AppRouter.tsx
+++ src/routes/AppRouter.tsx
@@ -1,10 +1,12 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-import DashboardPage from "@/pages/DashboardPage";
-import TransactionPage from "@/pages/TransactionPage";
-import PartnerPage from "@/pages/PartnerPage";
-import ReportPage from "@/pages/ReportPage";
+import DashboardPage from "@/pages/dashboard/DashboardPage";
+import PartnerPage from "@/pages/partner/PartnerPage";
+import ReportPage from "@/pages/report/ReportPage";
 import AppLayout from "@/layouts/AppLayout";
+import TransactionListPage from "@/pages/transactions/TransactionListPage";
+import TransactionFormPage from "@/pages/transactions/TransactionFormPage";
+import TransactionDetailPage from "@/pages/transactions/TransactionDetailPage";
 
 export default function AppRouter() {
   return (
@@ -12,7 +14,18 @@ export default function AppRouter() {
       <Routes>
         <Route element={<AppLayout />}>
           <Route path="/" element={<DashboardPage />} />
-          <Route path="/transactions" element={<TransactionPage />} />
+
+          <Route path="/transactions" element={<TransactionListPage />} />
+          <Route
+            path="/transactions/create"
+            element={<TransactionFormPage />}
+          />
+          <Route path="/transactions/:id" element={<TransactionDetailPage />} />
+          <Route
+            path="/transactions/:id/edit"
+            element={<TransactionFormPage />}
+          />
+
           <Route path="/partners" element={<PartnerPage />} />
           <Route path="/reports" element={<ReportPage />} />
         </Route>
